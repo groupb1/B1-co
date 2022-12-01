30394b3a479266c0a5a006b5bd42e6fda28f1676
Nooruddinkazi <119584709+Nooruddinkazi@users.noreply.github.com>
2022-12-01 15:47:05 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,33 @@
+function login(){
+    event.preventDefault();
+
+    var email = document.getElementById("se").value;
+    var password = document.getElementById("sp").value;
+
+    var i = emailArray.indexOf(email);
+
+    if(emailArray.indexOf(email) == -1){
+        if (email == ""){
+            alert("Username required.");
+            return ;
+        }
+        alert("Username does not exist.");
+        return ;
+    }
+    else if(passwordArray[i] != password){
+        if (password == ""){
+            alert("Password required.");
+            return ;
+        }
+        alert("Password does not match.");
+        return ;
+    }
+    else {
+        alert(email + " yor are login Now \n welcome to our website.");
+
+        document.getElementById("se").value ="";
+        document.getElementById("sp").value="";
+        return ;
+    }
+
+}
